b2cb53beaf5c9d5ae25ee1c562a48a214cb12732
Kayque Goldner <kayquegoldner@gmail.com>
2024-11-20 19:44:56 -0300

    user dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,46 @@
-import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs/server";
+import { PlusIcon } from "lucide-react";
 
-const DashboardPage =  async () => {
+import { db } from "@/db";
+import { DashboardPage } from "@/components/dashboard-page";
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal";
+
+import { DashboardPageContent } from "./dashboard-page-content";
+import { Button } from "@/components/ui/button";
+
+const Page =  async () => {
   const auth = await currentUser();
 
   if(!auth) {
     redirect("/sign-in");
   }
 
+  const user = await db.user.findUnique({
+    where: {
+      externalId: auth.id,
+    },
+  });
+
+  if(!user) {
+    redirect("/sign-in");
+  }
+
   return (
-    <div>
-      DashboardPage
-    </div>
+    <DashboardPage 
+      title="Dashboard"
+      cta={
+        <CreateEventCategoryModal>
+          <Button className="w-full sm:w-fit">
+            <PlusIcon className="size-4 mr-2" />
+            Add Category
+          </Button>
+        </CreateEventCategoryModal>
+      }
+    >
+      <DashboardPageContent />
+    </DashboardPage>
   )
 }
 
-export default DashboardPage;
+export default Page;
---
+++ src/server/routers/category-router.ts
@@ -0,0 +1,146 @@
+import { z } from "zod";
+import { startOfMonth } from "date-fns";
+
+import { db } from "@/db";
+import { parseColor } from "@/lib/utils";
+import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validator";
+
+import { router } from "../__internals/router";
+import { privateProcedure } from "../procedures";
+
+export const categoryRouter = router({
+  getEventCategories: privateProcedure
+    .query(async ({ c, ctx }) => {
+      const categories = await db.eventCategory.findMany({
+        where: { userId: ctx.user.id },
+        select: {
+          id: true,
+          name: true,
+          emoji: true,
+          color: true,
+          updatedAt: true,
+          createdAt: true,
+        },
+        orderBy: { updatedAt: "desc" },
+      });
+
+      const categoriesWithCounts = await Promise.all(categories.map(async (category) => {
+        const now = new Date();
+        const firstDayOfMonth = startOfMonth(now);
+
+        const [
+          uniqueFieldCount,
+          eventsCount,
+          lastPing
+        ] = await Promise.all([
+          db.event.findMany({
+            where: { 
+              EventCategory: { id: category.id },
+              createdAt: { gte: firstDayOfMonth },
+            },
+            select: { fields: true },
+            distinct: ["fields"],
+          }).then((events) => {
+            const fieldNames = new Set<string>();
+
+            events.forEach(event => {
+              Object.keys(event.fields as Object).forEach(fieldName => {
+                fieldNames.add(fieldName);
+              });
+            });
+
+            return fieldNames.size;
+          }),
+          db.event.count({
+            where: { 
+              EventCategory: { id: category.id },
+              createdAt: { gte: firstDayOfMonth },
+            },
+          }),
+          db.event.findFirst({
+            where: { EventCategory: { id: category.id } },
+            orderBy: { createdAt: "desc" },
+            select: { createdAt: true },
+          }),
+        ]);
+
+        return {
+          ...category,
+          uniqueFieldCount,
+          eventsCount,
+          lastPing: lastPing?.createdAt || null
+        }
+      }));
+
+      return c.superjson({ categories: categoriesWithCounts });
+    }),
+  
+  deleteCategory: privateProcedure
+    .input(z.object({ name: z.string() }))
+    .mutation(async ({ c, input, ctx }) => {
+      const { name } = input;
+
+      await db.eventCategory.delete({
+        where: { 
+          name_userId: { 
+            name,
+            userId: ctx.user.id,
+          },
+        },
+      });
+
+      return c.json({ success: true });
+    }),
+  
+  createEventCategory: privateProcedure
+    .input(z.object({
+      name: CATEGORY_NAME_VALIDATOR,
+      color: z.string().min(1, "Color is required").regex(/^#[0-9A-F]{6}$/i, "Invalid color format"),
+      emoji: z.string().emoji("Invalid emoji").optional()
+    }))
+    .mutation(async ({ c, ctx, input }) => {
+      const { user } = ctx;
+      const { color, name, emoji } = input;
+
+      // TODO: add paid plan logic
+      
+      const eventCategory = await db.eventCategory.create({
+        data: {
+          name: name.toLowerCase(),
+          color: parseColor(color),
+          emoji,
+          userId: user.id,
+        },
+      });
+
+      return c.json({ eventCategory });
+    }),
+
+  insertQuickstartCategories: privateProcedure
+    .mutation(async ({ ctx, c }) => {
+      const categories = await db.eventCategory.createMany({
+        data: [
+          {
+            name: "Bug",
+            emoji: "🐞",
+            color: 0XFF6B6B
+          },
+          {
+            name: "Sale",
+            emoji: "💰",
+            color: 0XFFEB3B
+          },
+          {
+            name: "Question",
+            emoji: "🤔",
+            color: 0X6C5CE7
+          },
+        ].map(category => ({
+          ...category,
+          userId: ctx.user.id,
+        })),
+      });
+
+      return c.json({ success: true, count: categories.count });
+    })
+});
